36ac18c7376c45d4b2141fb5c531b6370c7c57a9
CesarJardines <eduardroid.98@gmail.com>
2020-05-06 00:13:12 -0500

    cambios

--- static/js/script2.js
+++ static/js/script2.js
@@ -1,7 +1,7 @@
 const MAXIMOS_INTENTOS = 8, // Intentos máximos que tiene el jugador
     COLUMNAS = 4, // Columnas del memorama
     SEGUNDOS_ESPERA_VOLTEAR_IMAGEN = 1, // Por cuántos segundos mostrar ambas imágenes
-    NOMBRE_IMAGEN_OCULTA = "./img/question.png"; // La imagen que se muestra cuando la real está oculta
+    NOMBRE_IMAGEN_OCULTA = "/static/img/question.png"; // La imagen que se muestra cuando la real está oculta
 new Vue({
     el: "#app",
     data: () => ({
