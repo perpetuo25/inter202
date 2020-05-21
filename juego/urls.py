a6b65fdfe66a96bc1b78dffcd0d6c78d95ec4557
IxchelVe <ix.ve.ca@ciencias.unam.mx>
2020-05-21 18:19:27 -0500

    historia

--- juego/urls.py
+++ juego/urls.py
@@ -9,7 +9,21 @@ urlpatterns = [
 path('space/', views.space, name = "space"),
 path('tesoro/', views.tesoro, name = "tesoro"),
 path('levels/', views.levels, name = "levels"),
+
+path('rompecabezas/', views.rompecabezas, name = "rompecabezas"),
+path('memorama/', views.memorama, name = "memorama"),
 path('gato/', views.tictactoe, name = "gato"),
 path('simon/', views.simon, name = "simon"),
+path('encuentraCura/', views.encuentraCura, name = "encuentraCura"),
+path('sopaDeLetras/', views.sopaLetras, name = "sopaDeLetras"),
+
+path('gato/', views.tictactoe, name = "gato"),
+path('simon/', views.simon, name = "simon"),
+path('instruction/', views.inst, name = 'instruction'),
+path('gatohist/', views.gatohist, name = 'gatohist'),
+path('simonhist/', views.simonhist, name = 'simonhist'),
+path('', views.home, name = 'index'),
+
+
 ]
 
